e33a3afd976676e638c0994739ac59fcb034cecb
Nitin Kumar <78822174+kumar-nitin-tech@users.noreply.github.com>
2023-06-11 17:09:38 +0530

    Sort 012 code

--- dnfSort.cpp
+++ dnfSort.cpp
@@ -1,7 +1,5 @@
 /*
-    problem statement : Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
-
-    problem link : https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+   problem link : https://www.codingninjas.com/codestudio/problems/sort-0-1-2_8230695?challengeSlug=striver-sde-challenge
 */
 #include<bits/stdc++.h>
 using namespace std;
